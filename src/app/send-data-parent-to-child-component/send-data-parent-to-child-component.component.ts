4b8243c37d572cbaf5d1ea3294d6b56000515a26
Talha Zubair <talha@redfireai.com>
2022-05-12 15:26:34 +0500

    Re UseAble Component

--- src/app/send-data-parent-to-child-component/send-data-parent-to-child-component.component.ts
+++ src/app/send-data-parent-to-child-component/send-data-parent-to-child-component.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SendDataParentToChildComponentComponent implements OnInit {
   constructor() {}
+  // @Input() Item: any = [];
   @Input() Item: any = [];
   ngOnInit(): void {}
 }

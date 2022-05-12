0769cebc192398c121807208713dc55bcd73aa51
Talha Zubair <talha@redfireai.com>
2022-05-12 12:19:07 +0500

    Dynamic Style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
-import {UserAuthModule} from "./user-auth/user-auth.module";
+import { UserAuthModule } from './user-auth/user-auth.module';
 import { MakeAndCallFunctionComponent } from './make-and-call-function/make-and-call-function.component';
 import { ImportantEventsComponent } from './important-events/important-events.component';
 import { MakeACounterComponent } from './make-acounter/make-acounter.component';
@@ -14,9 +14,23 @@ import { IfElseConditionComponent } from './if-else-condition/if-else-condition.
 import { SwitchCasesinAngularComponent } from './switch-casesin-angular/switch-casesin-angular.component';
 import { ForLoopInAngularComponent } from './for-loop-in-angular/for-loop-in-angular.component';
 import { NestedLoopAngularComponent } from './nested-loop-angular/nested-loop-angular.component';
+import { DynamicStyleComponent } from './dynamic-style/dynamic-style.component';
 @NgModule({
-  declarations: [AppComponent, UserListComponent, MakeAndCallFunctionComponent, ImportantEventsComponent, MakeACounterComponent, BasicRuleOfStyleComponent, PropertyBindingComponent, IfElseConditionComponent, SwitchCasesinAngularComponent, ForLoopInAngularComponent, NestedLoopAngularComponent, ],
-  imports: [BrowserModule, AppRoutingModule,UserAuthModule],
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    MakeAndCallFunctionComponent,
+    ImportantEventsComponent,
+    MakeACounterComponent,
+    BasicRuleOfStyleComponent,
+    PropertyBindingComponent,
+    IfElseConditionComponent,
+    SwitchCasesinAngularComponent,
+    ForLoopInAngularComponent,
+    NestedLoopAngularComponent,
+    DynamicStyleComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, UserAuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })

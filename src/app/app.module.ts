57c3a2456fb4c7fd3152e237c55279f2f0c8e3e0
Talha Zubair <talha@redfireai.com>
2022-05-12 10:38:05 +0500

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,9 @@ import {UserAuthModule} from "./user-auth/user-auth.module";
 import { MakeAndCallFunctionComponent } from './make-and-call-function/make-and-call-function.component';
 import { ImportantEventsComponent } from './important-events/important-events.component';
 import { MakeACounterComponent } from './make-acounter/make-acounter.component';
+import { BasicRuleOfStyleComponent } from './basic-rule-of-style/basic-rule-of-style.component';
 @NgModule({
-  declarations: [AppComponent, UserListComponent, MakeAndCallFunctionComponent, ImportantEventsComponent, MakeACounterComponent, ],
+  declarations: [AppComponent, UserListComponent, MakeAndCallFunctionComponent, ImportantEventsComponent, MakeACounterComponent, BasicRuleOfStyleComponent, ],
   imports: [BrowserModule, AppRoutingModule,UserAuthModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/basic-rule-of-style/basic-rule-of-style.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-rule-of-style',
+  templateUrl: './basic-rule-of-style.component.html',
+  styleUrls: ['./basic-rule-of-style.component.css']
+})
+export class BasicRuleOfStyleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

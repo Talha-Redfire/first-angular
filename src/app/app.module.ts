897cd7d61db59872a3497092e7fe5c814f99918d
Talha Zubair <talha@redfireai.com>
2022-05-12 09:37:49 +0500

    Modules in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
-
+import {UserAuthModule} from "./user-auth/user-auth.module"
 @NgModule({
   declarations: [AppComponent, UserListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule,UserAuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })

cc65fd7b65fc6a3cf8c424dc1658483a3e636069
Talha Zubair <talha@redfireai.com>
2022-05-12 09:28:54 +0500

    component with inline style

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  // styleUrls: ['./user-list.component.css']
+  styles: [
+    `
+      sstyle {
+        color: red;
+      }
+    `,
+  ],
 })
 export class UserListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

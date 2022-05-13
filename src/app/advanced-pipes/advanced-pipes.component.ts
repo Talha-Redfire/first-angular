821cf3e3bcf5c3773ba7051d2b4796dfedc56afb
Talha Zubair <talha@redfireai.com>
2022-05-13 09:46:28 +0500

    Advanced pipes

--- src/app/advanced-pipes/advanced-pipes.component.ts
+++ src/app/advanced-pipes/advanced-pipes.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-advanced-pipes',
   templateUrl: './advanced-pipes.component.html',
-  styleUrls: ['./advanced-pipes.component.css']
+  styleUrls: ['./advanced-pipes.component.css'],
 })
 export class AdvancedPipesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  name: string = 'AdvancedPipes';
+  person: {
+    name: string;
+    age: Number;
+  } = {
+    name: 'John',
+    age: 10,
+  };
+  ngOnInit(): void {}
 }
